a05f92e8c383da67c6deac9a990f603ed454b56f
vikki <vigneshvelusamy@gmail.com>
2020-07-01 12:03:13 +0530

    Steerwise master code.

---
+++ src/main/java/com/org/servease/ServeaseApplication.java
@@ -0,0 +1,13 @@
+package com.org.servease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/servease/ServeaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.servease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
